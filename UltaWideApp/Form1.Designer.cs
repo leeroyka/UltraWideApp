34626505241971be89d36576c3045cd7781812e7
leeroyka <blokhin321@gmail.com>
2020-04-30 15:27:42 +0300

    Clone Rep

---
+++ UltaWideApp/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace UltaWideApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(201, 303);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(307, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(132, 147);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(308, 171);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(132, 147);
+            this.listBox3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Добавить>>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(210, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Удалить<<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(210, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Удалить<<";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "UltraWideApp";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(211, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(23, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "R";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 330);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "UltaWideApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ UltaWideApp/Form1.cs
@@ -0,0 +1,449 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace UltaWideApp
+{
+    
+    public partial class Form1 : Form
+    {
+        private class W
+        {
+            public String name;
+            public int index = -1;
+            public W(String _name)
+            {
+                name = _name;
+            }
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KBDLLHOOKSTRUCT
+        {
+            public Keys key;
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(
+            int id, LowLevelKeyboardProc callback, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(
+            IntPtr hook, int nCode, IntPtr wp, IntPtr lp);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string name);
+
+        private IntPtr ptrHook;
+
+        private LowLevelKeyboardProc objKeyboardProcess;
+
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref Rect lpRect);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        Process[] prArray;
+        List<W> left;
+        List<W> right;
+        public Form1()
+        {
+            InitializeComponent();
+
+            const string applicationName = "UltraWideApp";
+            const string pathRegistryKeyStartup =
+                        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+            using (RegistryKey registryKeyStartup =
+                        Registry.CurrentUser.OpenSubKey(pathRegistryKeyStartup, true))
+            {
+                registryKeyStartup.SetValue(
+                    applicationName,
+                    string.Format("\"{0}\"", System.Reflection.Assembly.GetExecutingAssembly().Location));
+                registryKeyStartup.Flush();
+            }
+
+            notifyIcon1.Visible = false;
+            this.notifyIcon1.MouseClick += new MouseEventHandler(notifyIcon1_MouseClick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ProcessModule objCurrentModule =
+                Process.GetCurrentProcess().MainModule;
+            objKeyboardProcess = new LowLevelKeyboardProc(captureKey);
+            ptrHook = SetWindowsHookEx(13, objKeyboardProcess,
+                GetModuleHandle(objCurrentModule.ModuleName), 0);
+
+
+            prArray = Process.GetProcesses();
+            int i = 0;
+            String pr = "brave";
+
+            left = new List<W>();
+            right = new List<W>();
+            //left.Add(new W("brave"));
+            //left.Add(new W("devenv"));
+            //left.Add(new W("steam"));
+            //right.Add(new W("Telegram"));
+            ReadFromFile();
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+
+            foreach (var a in left)
+            {
+                listBox2.Items.Add(a.name);
+            }
+            foreach (var a in right)
+            {
+                listBox3.Items.Add(a.name);
+            }
+            foreach (var a in prArray)
+            {
+                if (a.MainWindowHandle.ToString() != "0")
+                {
+                    listBox1.Items.Add(a.ToString().Substring(28).Remove(a.ToString().Length-1-28));
+                    foreach (var b in left)
+                    {
+                        //Console.WriteLine(a.ToString());
+
+                        if (a.ToString().IndexOf(b.name) >= 0)
+                        {
+                            b.index = i;
+                        }
+                    }
+                    foreach (var b in right)
+                    {
+                       // Console.WriteLine(a.ToString());
+                        if (a.ToString().IndexOf(b.name) >= 0)
+                        {
+                            b.index = i;
+                        }
+                    }
+                }
+                i++;
+
+            }
+            foreach (var b in left)
+            {
+                if (b.index != -1)
+                {
+                    IntPtr hWnd = prArray[b.index].MainWindowHandle;
+                    Rect rct = new Rect();
+                    GetWindowRect(hWnd, ref rct);
+                    //Console.WriteLine("");
+                    //Console.WriteLine(rct.Left.ToString() + " " + rct.Top.ToString() + " " + rct.Right.ToString() + " " + rct.Bottom.ToString());
+
+                    SetWindowPos(hWnd, 0,
+                        0,
+                        0,
+                        1920,
+                        1040,
+                        0x0040);
+                    if (b.name == "brave")
+                        SetWindowPos(hWnd, 0,
+                        -7,
+                        0,
+                        1934,
+                        1048,
+                        0x0040);
+
+                }
+            }
+            foreach (var b in right)
+            {
+                if (b.index != -1)
+                {
+                    IntPtr hWnd = prArray[b.index].MainWindowHandle;
+                    Rect rct = new Rect();
+                    GetWindowRect(hWnd, ref rct);
+                    //Console.WriteLine("");
+                    //Console.WriteLine(rct.Left.ToString() + " " + rct.Top.ToString() + " " + rct.Right.ToString() + " " + rct.Bottom.ToString());
+
+                    SetWindowPos(hWnd, 0,
+                    1920,
+                    0,
+                    640,
+                    1040,
+                    0x0040);
+
+                }
+            }
+
+            this.WindowState = FormWindowState.Minimized;
+
+            // прячем наше окно из панели
+            this.ShowInTaskbar = false;
+            // делаем нашу иконку в трее активной
+            notifyIcon1.Visible = true;
+        }
+        void SetWindows()
+        {
+            foreach (var b in left)
+            {
+                if (b.index != -1)
+                {
+                    IntPtr hWnd = prArray[b.index].MainWindowHandle;
+                    Rect rct = new Rect();
+                    GetWindowRect(hWnd, ref rct);
+                    //Console.WriteLine("");
+                    //Console.WriteLine(rct.Left.ToString() + " " + rct.Top.ToString() + " " + rct.Right.ToString() + " " + rct.Bottom.ToString());
+                    
+                    SetWindowPos(hWnd, 0,
+                        0,
+                        0,
+                        1920,
+                        1040,
+                        0x0040);
+                    if (b.name == "brave")
+                        SetWindowPos(hWnd, 0,
+                        -7,
+                        0,
+                        1934,
+                        1048,
+                        0x0040);
+
+                }
+            }
+            foreach (var b in right)
+            {
+                if (b.index != -1)
+                {
+                    IntPtr hWnd = prArray[b.index].MainWindowHandle;
+                    Rect rct = new Rect();
+                    GetWindowRect(hWnd, ref rct);
+
+                    SetWindowPos(hWnd, 0,
+                    1920,
+                    0,
+                    640,
+                    1040,
+                    0x0040);
+
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(listBox2.Items.IndexOf(listBox1.SelectedItem.ToString())==-1 && listBox3.Items.IndexOf(listBox1.SelectedItem.ToString()) == -1)
+            {
+                left.Add(new W(listBox1.SelectedItem.ToString()));
+                listBox2.Items.Add(listBox1.SelectedItem.ToString());
+                SaveToFile();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.IndexOf(listBox1.SelectedItem.ToString()) == -1 && listBox3.Items.IndexOf(listBox1.SelectedItem.ToString()) == -1)
+            {
+                right.Add(new W(listBox1.SelectedItem.ToString()));
+                listBox3.Items.Add(listBox1.SelectedItem.ToString());
+                SaveToFile();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.SelectedItem.ToString();
+            for(int i=0;i<left.Count;i++)
+            {
+                if(left[i].name== listBox2.SelectedItem.ToString())
+                {
+                    left.RemoveAt(i);
+                    break;
+                }
+            }
+            listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            SaveToFile();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox3.SelectedItem.ToString();
+            for (int i = 0; i < right.Count; i++)
+            {
+                if (right[i].name == listBox3.SelectedItem.ToString())
+                {
+                    right.RemoveAt(i);
+                    break;
+                }
+            }
+            listBox3.Items.RemoveAt(listBox3.SelectedIndex);
+            SaveToFile();
+        }
+
+        string pathLeft = @"left.txt";
+        string pathRight = @"right.txt";
+        void ReadFromFile()
+        {
+            using (StreamReader sr = new StreamReader(pathLeft, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    left.Add(new W(line));
+                }
+            }
+            using (StreamReader sr = new StreamReader(pathRight, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    right.Add(new W(line));
+                }
+            }
+
+        }
+        void SaveToFile()
+        {
+            
+            using (StreamWriter sw = new StreamWriter(pathLeft, false, System.Text.Encoding.Default))
+            {
+                foreach(var a in left)
+                {
+                    sw.WriteLine(a.name);
+                }
+             // sw.WriteLine(text);
+            }
+            using (StreamWriter sw = new StreamWriter(pathRight, false, System.Text.Encoding.Default))
+            {
+                foreach (var a in right)
+                {
+                    sw.WriteLine(a.name);
+                }
+                // sw.WriteLine(text);
+            }
+
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if ($"{ e.KeyCode}" == "C" && $"{e.Modifiers}" == "Alt")
+            //{
+            //    label1.Text = "lol";
+            //}
+           // label1.Text = e.Modifiers.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+
+        private IntPtr captureKey(int nCode, IntPtr wp, IntPtr lp)
+        {
+            if (nCode >= 0)
+            {
+                KBDLLHOOKSTRUCT objKeyInfo =
+                    (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lp, typeof(KBDLLHOOKSTRUCT));
+                if (objKeyInfo.key == Keys.Pause)
+                {
+                    //return (IntPtr)1;
+                    //label1.Text = "lol";
+                    SetWindows();
+                }
+            }
+            return CallNextHookEx(ptrHook, nCode, wp, lp);
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // проверяем наше окно, и если оно было свернуто, делаем событие        
+            if (WindowState == FormWindowState.Minimized)
+            {
+                // прячем наше окно из панели
+                this.ShowInTaskbar = false;
+                // делаем нашу иконку в трее активной
+                notifyIcon1.Visible = true;
+            }
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // делаем нашу иконку скрытой
+            notifyIcon1.Visible = false;
+            // возвращаем отображение окна в панели
+            this.ShowInTaskbar = true;
+            //разворачиваем окно
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // делаем нашу иконку скрытой
+            notifyIcon1.Visible = false;
+            // возвращаем отображение окна в панели
+
+            WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            //разворачиваем окно
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            prArray = Process.GetProcesses();
+            int i = 0;
+            listBox1.Items.Clear();
+            foreach (var a in prArray)
+            {
+                if (a.MainWindowHandle.ToString() != "0")
+                {
+                    listBox1.Items.Add(a.ToString().Substring(28).Remove(a.ToString().Length - 1 - 28));
+                    foreach (var b in left)
+                    {
+                        //Console.WriteLine(a.ToString());
+
+                        if (a.ToString().IndexOf(b.name) >= 0)
+                        {
+                            b.index = i;
+                        }
+                    }
+                    foreach (var b in right)
+                    {
+                        // Console.WriteLine(a.ToString());
+                        if (a.ToString().IndexOf(b.name) >= 0)
+                        {
+                            b.index = i;
+                        }
+                    }
+                }
+                i++;
+
+            }
+
+        }
+    }
+}
